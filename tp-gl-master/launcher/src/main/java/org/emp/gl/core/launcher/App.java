2883f6347e4f106df3d0706c71a8d8a309efd132
manel <manelarama0@gmail.com>
2025-11-06 10:40:40 +0100

    realisation de la partie a/b/c

--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -1,11 +1,9 @@
 package org.emp.gl.core.launcher;
-
+import org.emp.gl.time.service.impl.DummyTimeServiceImpl;
 import org.emp.gl.clients.Horloge ;
+import org.emp.gl.timer.service.TimerService;
 
-/**
- * Hello world!
- *
- */
+
 public class App {
 
     public static void main(String[] args) {
@@ -14,11 +12,32 @@ public class App {
     }
 
     private static void testDuTimeService() {
-        Horloge horloge = new Horloge("Num 1") ;
+        //1. instanciation du service de temps 
+        TimerService timerService = new  DummyTimeServiceImpl();
+        
+        // 2. Instancier plusieurs horloges avec injection du service
+        Horloge horloge1 = new Horloge("Salon", timerService);
+        Horloge horloge2 = new Horloge("Cuisine", timerService);
+        Horloge horloge3 = new Horloge("Chambre", timerService);
+        // Use horloge3 so the local variable is not reported as unused
+        System.out.println(horloge3);
+
+       // 3. Laisser tourner pendant un moment pour observer le résultat
+        try {
+            // Attendre 30 secondes pour voir plusieurs mises à jour
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("Fin du test");
     }
+    
 
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
+
+
 }
--- tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/Horloge.java
+++ tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/Horloge.java
@@ -1,20 +1,46 @@
 package org.emp.gl.clients ; 
+import org.emp.gl.timer.service.TimerService ;
+import java.beans.PropertyChangeEvent;
+import org.emp.gl.timer.service.TimeChangeProvider;
+import org.emp.gl.timer.service.TimerChangeListener;
 
-import org.emp.gl.timer.service.TimerService ; 
-
-
-public class Horloge {
-
+public class Horloge implements TimerChangeListener{
+ 
     String name; 
     TimerService timerService ; 
 
 
-    public Horloge (String name) {
+    public Horloge (String name, TimerService timerService) {
         this.name = name ; 
-
+         this.timerService = timerService;
+         // S'inscrire comme listener auprès du TimerService
+        this.timerService.addTimeChangeListener(this);
         System.out.println ("Horloge "+name+" initialized!") ;
     }
 
+    @Override
+    public void propertyChange(String prop, Object oldValue, Object newValue) {
+        // Afficher l'heure à chaque changement de seconde
+        if (SECONDE_PROP.equals(prop)) {
+            afficherHeure();
+        }
+        // Vous pouvez aussi gérer les autres types de changement
+        else if (MINUTE_PROP.equals(prop)) {
+            System.out.println(name + " - Minute changée: " + oldValue + " → " + newValue);
+            afficherHeure();
+        }
+        else if (HEURE_PROP.equals(prop)) {
+            System.out.println(name + " - Heure changée: " + oldValue + " → " + newValue);
+            afficherHeure();
+        }
+    }
+    
+  public void arreter() {
+        if (timerService != null) {
+            this.timerService.removeTimeChangeListener(this);
+        }
+    }
+  
     public void afficherHeure () {
         if (timerService != null)
             System.out.println (name + " affiche " + 
